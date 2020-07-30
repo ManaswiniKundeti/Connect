4305effb21c9669fb320ce99402aab6a4d3792af
ManaswiniKundeti <manaswini.kundeti@gmail.com>
2020-07-30 13:33:18 -0400

    Created thee Fragments to display in TabLayout using ViewPager.

---
+++ app/src/main/java/com/manu/connect/view/ui/activities/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.manu.connect.view.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.manu.connect.R
+import com.manu.connect.view.ui.fragments.ChatFragment
+import com.manu.connect.view.ui.fragments.SearchFragment
+import com.manu.connect.view.ui.fragments.SettingsFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar_main))
+
+        val toolbar:Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
+
+        val tabLayout :TabLayout = findViewById(R.id.tab_layout_main)
+        val viewPager :ViewPager = findViewById(R.id.view_pager_main)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.addFragment(ChatFragment(),"Chats")
+        viewPagerAdapter.addFragment(SearchFragment(), "Search")
+        viewPagerAdapter.addFragment(SettingsFragment(), "Settings")
+
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    internal class ViewPagerAdapter(fragmentManager : FragmentManager) :
+        FragmentPagerAdapter(fragmentManager){
+
+        private val fragments : ArrayList<Fragment> = ArrayList<Fragment>()
+        private val titles : ArrayList<String> = ArrayList<String>()
+
+        /**
+         * Return the Fragment associated with a specified position.
+         */
+        override fun getItem(position: Int): Fragment {
+            return  fragments[position]
+        }
+
+        /**
+         * Return the number of views available.
+         */
+        override fun getCount(): Int {
+            return fragments.size
+        }
+
+        fun addFragment(fragment: Fragment, title : String){
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titles[position]
+        }
+
+    }
+
+}
