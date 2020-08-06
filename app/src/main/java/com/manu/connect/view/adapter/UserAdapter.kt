5225caf478f0e77fa92b815219d2a30606ba0bc3
ManaswiniKundeti <manaswini.kundeti@gmail.com>
2020-08-06 10:24:30 -0400

    Implemented search contacts feature

---
+++ app/src/main/java/com/manu/connect/view/adapter/UserAdapter.kt
@@ -0,0 +1,56 @@
+package com.manu.connect.view.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+import com.manu.connect.R
+import com.manu.connect.model.Users
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.search_item.view.*
+
+class UserAdapter(mContext: Context,
+                  mUsers : List<Users>,
+                  isChatCheck : Boolean) : RecyclerView.Adapter<ItemViewHolder>() {
+
+    private val mContext : Context = mContext
+    private val mUsers : List<Users> = mUsers
+    private val isChatCheck : Boolean = isChatCheck
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ItemViewHolder {
+        val view : View = LayoutInflater.from(mContext).inflate(R.layout.search_item, viewGroup, false)
+        return ItemViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bindView(mUsers[position])
+    }
+
+
+    override fun getItemCount(): Int {
+        return mUsers.size
+    }
+
+}
+class ItemViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    //        var usernameTextView : TextView = itemView.findViewById(R.id.username_search_item)
+//        var profileImageView : CircleImageView = itemView.findViewById(R.id.profile_image_search_item)
+//        var onlineImageView : CircleImageView = itemView.findViewById(R.id.image_online_search_item)
+//        var offlineImageView : CircleImageView = itemView.findViewById(R.id.image_offline_search_item)
+//        var lastMessageTextView : TextView = itemView.findViewById(R.id.message_last_search_item)
+//
+    fun bindView(user : Users){
+        itemView.username_search_item.text = user.getUsername()
+
+        Picasso.get().load(user.getProfile())
+            .placeholder(R.drawable.profile_image)
+            .into(itemView.profile_image_search_item)
+    }
+}
--- app/src/main/java/com/manu/connect/view/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/manu/connect/view/ui/fragments/SearchFragment.kt
@@ -1,60 +1,118 @@
 package com.manu.connect.view.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.manu.connect.R
+import com.manu.connect.model.Users
+import com.manu.connect.view.adapter.UserAdapter
+import kotlinx.android.synthetic.main.fragment_search.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var userAdapter : UserAdapter? = null
+    private var mUsers : List<Users>? = null
+    private var mSearchEditText : EditText? = null
+    private var mRecyclerView : RecyclerView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_search, container, false)
+
+        mRecyclerView = view.findViewById(R.id.search_list)
+        mRecyclerView?.setHasFixedSize(true)
+        mRecyclerView?.layoutManager = LinearLayoutManager(context)
+
+        mUsers = ArrayList()
+        retrieveAllUsers()
+
+        mSearchEditText = view.findViewById(R.id.search_users_edit_text)
+
+        mSearchEditText!!.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(charSequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                searchUser(charSequence.toString().toLowerCase())
+            }
+
+        })
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun retrieveAllUsers() {
+        var firebaseUserID = FirebaseAuth.getInstance().currentUser!!.uid
+        val refUsers = FirebaseDatabase.getInstance().reference.child("Users")
+
+        refUsers.addValueEventListener(object : ValueEventListener {
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (mUsers as ArrayList<Users>).clear()
+                if(mSearchEditText?.text.toString() == ""){
+                    for(currentSnapshot in snapshot.children){
+                        val user : Users? = currentSnapshot.getValue(Users::class.java)
+                        if(user!!.getUID() != firebaseUserID){
+                            (mUsers as ArrayList<Users>).add(user)
+                        }
+                    }
+                    userAdapter = UserAdapter(context!!, mUsers!!, false)
+                    mRecyclerView!!.adapter = userAdapter
                 }
             }
+
+        })
+    }
+
+    private fun searchUser(name : String){
+        var firebaseUserID = FirebaseAuth.getInstance().currentUser!!.uid
+        val queryUsers = FirebaseDatabase.getInstance().reference
+            .child("Users")
+            .orderByChild("search")
+            .startAt(name)
+            .endAt(name + "\uf8ff")
+
+        queryUsers.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (mUsers as ArrayList<Users>).clear()
+
+                for(currentsSnapshot in snapshot.children){
+                    val user : Users? = currentsSnapshot.getValue(Users::class.java)
+                    if(user!!.getUID() != firebaseUserID){
+                        (mUsers as ArrayList<Users>).add(user)
+                    }
+                }
+                userAdapter = UserAdapter(context!!, mUsers!!, false)
+                mRecyclerView!!.adapter = userAdapter
+            }
+
+        })
+
+
     }
 }
